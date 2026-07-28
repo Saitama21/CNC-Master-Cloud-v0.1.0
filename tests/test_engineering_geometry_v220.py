994cea6dabc2298f8cbd65eb184dad99a428041c
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-07-29 01:41:14 +0300

    Add files via upload

---
+++ tests/test_engineering_geometry_v220.py
@@ -0,0 +1,17 @@
+from app.client_ui import CLIENT_HTML
+
+
+def test_v220_engineering_geometry_controls_present():
+    assert "v2.2.0" in CLIENT_HTML
+    assert 'id="workMode"' in CLIENT_HTML
+    assert 'value="turn"' in CLIENT_HTML
+    assert 'value="mill"' in CLIENT_HTML
+    assert 'id="buildGeometry"' in CLIENT_HTML
+    assert 'id="geoWidth"' in CLIENT_HTML
+    assert 'id="geoFlat"' in CLIENT_HTML
+    assert 'id="geoRadius"' in CLIENT_HTML
+
+
+def test_v220_prevents_turning_gcode_for_xy_mode():
+    assert "state.workMode==='mill'" in CLIENT_HTML
+    assert "токарный MPF не создаётся" in CLIENT_HTML
